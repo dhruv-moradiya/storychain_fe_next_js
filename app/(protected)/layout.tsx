a4ace85946b8a06f54366f17527c66c604892629
Dhruv Moradiya <dmoradiya443@gmail.com>
2026-02-07 20:27:51 +0530

    refactor: restructure protected routes and optimize stories layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/common/navbar';
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  console.log('Protected Layout');
   return (
     <>
       <Navbar />
